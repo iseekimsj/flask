9fd1803e4bb848ce3610eee6da54ce6e5db082e9
iseekimsj <iseekimsj@gmail.com>
2019-08-10 18:21:59 +0900

    190810 18:20

---
+++ textmining/__init__.py
@@ -0,0 +1,11 @@
+from textmining.model import SamsungReport
+
+if __name__ == '__main__':
+    sam = SamsungReport()
+    # sam.download()
+
+    # print(sam.extract_noun())
+    # print(sam.read_stopword())
+    print(sam.find_freq())
+    sam.draw_wordcloud()
+
